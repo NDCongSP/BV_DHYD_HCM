25d836a9601385df25bfa94eefa6b533f33da9bf
NDCongSP <ndcong08cddv02@gmail.com>
2025-09-10 18:51:13 +0700

    up

--- 20250810_SirenCenter/SirenCenter/SirenCenter/model/DataModel.cs
+++ 20250810_SirenCenter/SirenCenter/SirenCenter/model/DataModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,11 @@ namespace SirenCenter
     public class DataModel
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime CreateAt { get; set; }
         public int LocationId { get; set; }
+        public string LocationName { get; set; }
         public string Value { get; set; }
         public string Name { get; set; }
         public double LowLevel { get; set; }
